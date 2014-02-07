9cc93fe1540eecd49128f11ca46ab9cf0834bfe5
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2014-02-07 11:27:57 -0500

    Removed model refs

--- components/codeArea/index.js
+++ components/codeArea/index.js
@@ -7,9 +7,5 @@ exports.init = function(model) {
 // The create function is called after the component is created
 // and has been added to the DOM. It only runs in the browser
 exports.create = function(model, dom) {
-	var path, self = this;
-
-	path = model.get('path');
-	
-	return model.ref('_code', 'path');
+	var self = this;
 }
